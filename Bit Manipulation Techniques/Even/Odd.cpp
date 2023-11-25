1a18eb3d95c005a3ad7ff9619babedd6da038aba
Diya <diya.solanki.31@gmail.com>
2023-11-25 18:57:52 +0530

    Program Done

---
+++ Bit Manipulation Techniques/Even/Odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   cin>>x;
+   if(x&1){
+       cout<<"\n Odd";
+       
+   }
+   else
+   cout<<"\n Even";
+
+    return 0;
+}
